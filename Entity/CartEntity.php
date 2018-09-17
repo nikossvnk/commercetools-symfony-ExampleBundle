51edfa7602377af4976b66b3bdea12edc463aaa1
nikos.sotiropoulos <nikolaos.sotiropoulos@commercetools.de>
2018-09-17 13:38:46 +0200

    WIP: improvements on payments actions

---
+++ Entity/CartEntity.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ */
+
+namespace Commercetools\Symfony\ExampleBundle\Entity;
+
+
+use Commercetools\Core\Model\Cart\Cart;
+
+class CartEntity
+{
+    /**
+    public $billingAddress;
+
+    public $shippingAddress;
+
+    public $check;
+
+    public $name;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public static function ofCart(Cart $cart)
+    {
+        $entity = new static();
+        $entity->shippingAddress = !is_null($cart->getShippingAddress()) ? $cart->getShippingAddress()->toArray() : [];
+        $entity->billingAddress = !is_null($cart->getBillingAddress()) ? $cart->getBillingAddress()->toArray() : [];
+
+        return $entity;
+    }
+}
